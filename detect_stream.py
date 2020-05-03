5a8d41c4dbda783607c319e0e079c86e18df80ac
chattypus <chaitanya@locus.sh>
2020-05-03 17:29:40 +0530

    Add perspective

--- detect_stream.py
+++ detect_stream.py
@@ -3,6 +3,7 @@ from torchvision import transforms
 from utils import *
 from PIL import Image, ImageDraw, ImageFont
 import cv2
+from scipy.spatial.distance import squareform
 import numpy as np
 import time
 # from imutils.video import FPS
@@ -140,7 +141,31 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
 if __name__ == '__main__':
 
     video_path = 'oxford_dataset/TownCentreXVID.avi'
-    # video_path =''
+    rect = np.array([[739, 119], [990, 148], [614, 523], [229, 437]], np.float32)
+
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    # ...and now for the height of our new image
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    # take the maximum of the width and height values to reach
+    # our final dimensions
+    maxWidth = max(int(widthA), int(widthB))
+    maxHeight = max(int(heightA), int(heightB))
+    # construct our destination points which will be used to
+    # map the screen to a top-down, "birds eye" view
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    # calculate the perspective transform matrix and warp
+    # the perspective to grab the screen
+    M = cv2.getPerspectiveTransform(rect, dst)
+
+
+    # video_path ='oxford_dataset/Dataset2.mp4'
     vc = cv2.VideoCapture(video_path)
     total_frames = int(vc.get(cv2.CAP_PROP_FRAME_COUNT))
     print(total_frames)
@@ -151,37 +176,52 @@ if __name__ == '__main__':
 
     while success:
         success, image = vc.read()
+        if success:
+            # You may need to convert the color.
 
-        # You may need to convert the color.
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        im_pil = Image.fromarray(img)
-        # img_path = 'oxford_dataset/extracted_images/frame_3743.jpg'
-        # original_image = Image.open(img_path, mode='r')
-        original_image = im_pil.convert('RGB')
-        start = time.time()
-        det_boxes, det_labels, det_scores = get_boxes_labels(original_image, min_score=0.2, max_overlap=0.5, top_k=200)
-        time_taken = time.time() - start
-        print(time_taken)
-        # print(det_labels)
-        # a = detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200)
-        # numpy_image = np.array(a)
-        #
-        # # convert to a openCV2 image, notice the COLOR_RGB2BGR which means that
-        # # the color is converted from RGB to BGR format
-        # opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
-        # cv2.imshow('test', opencv_image)
-        for i in range(det_boxes.size(0)):
-            box = det_boxes[i].tolist()
-            cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, thickness)
+            img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            im_pil = Image.fromarray(img)
+            # img_path = 'oxford_dataset/extracted_images/frame_3743.jpg'
+            # original_image = Image.open(img_path, mode='r')
+            original_image = im_pil.convert('RGB')
+            start = time.time()
+            det_boxes, det_labels, det_scores = get_boxes_labels(original_image, min_score=0.3, max_overlap=0.4, top_k=100)
+            time_taken = time.time() - start
+            print(time_taken)
+
+            centers = torch.transpose(torch.stack(((det_boxes[:, 0] + det_boxes[:, 2])/2, (det_boxes[:, 1] + det_boxes[:, 3])/2)), 0, 1)
+
+            dist_mat = torch.nn.functional.pdist(centers)
+            dist_mat = torch.from_numpy(squareform(dist_mat.detach().numpy()))
+
+            # print(det_labels)
+            # a = detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200)
+            # numpy_image = np.array(a)
+            #
+            # # convert to a openCV2 image, notice the COLOR_RGB2BGR which means that
+            # # the color is converted from RGB to BGR format
+            # opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+            # cv2.imshow('test', opencv_image)
+
+            for i in range(det_boxes.size(0)):
+                color = (255, 0, 0)
+                box = det_boxes[i].tolist()
+                a = dist_mat[:, i] < 100:
+                if torch.sum(a.type(torch.ByteTensor)) > 1:
+                    color = (0, 0, 255)
+                cv2.rectangle(image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), color, thickness)
+                cv2.circle(image, tuple(centers[i,:]), 5, (255, 255, 0), 10)
+                image_2 = cv2.warpPerspective(image, M, (1920, 1080))
 
 
-        cv2.imshow('test', image)
-        key = cv2.waitKey(1) & 0xFF
-        if key & 0xFF == ord('q'):
-            break
+            cv2.imshow('test', image)
+            cv2.imshow('test2', image_2)
+            key = cv2.waitKey(1) & 0xFF
+            if key & 0xFF == ord('q'):
+                break
 
-        if key == ord('n'):
-            continue
+            if key == ord('n'):
+                continue
 
     cv2.destroyAllWindows()
 
